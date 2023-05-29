214d0f941f7ffa29409f9f8f16d945c3094191dc
Forkeh <forkeh@gmail.com>
2023-05-29 19:22:11 +0200

    solved

--- environments/environment-01/script.js
+++ environments/environment-01/script.js
@@ -1,2 +1,40 @@
 "use strict";
 
+window.addEventListener("load", app);
+
+async function app() {
+    const users = await getData();
+    showUsers(users);
+    console.log(users);
+
+    document.querySelector("#admin-count").textContent = `${
+        countRole("admin", users).length
+    }`;
+    document.querySelector("#user-count").textContent = `${
+        countRole("user", users).length
+    }`;
+    document.querySelector("#guest-count").textContent = `${
+        countRole("guest", users).length
+    }`;
+}
+
+async function getData() {
+    const data = await fetch("users.json");
+
+    return data.json();
+}
+
+function showUsers(users) {
+    for (const user of users) {
+        const html = /*html*/ `
+<ul>${user.name}, ${user.role}, ${user.active}</ul>
+`;
+        document
+            .querySelector("#userlist")
+            .insertAdjacentHTML("beforeend", html);
+    }
+}
+
+function countRole(role, users) {
+    return users.filter(user => user.role === role);
+}
